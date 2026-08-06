5cc4e6138a12d2fc39770f1d6a35bba860d8283c
Adem Gabsi <adem.gabsi@esprit.tn>
2026-08-06 10:33:37 +0100

    feat(commun): ajoute filtre global de gestion des erreurs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FiltreExceptionHttp } from './commun/filtres/filtre-exception-http.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalFilters(new FiltreExceptionHttp());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
